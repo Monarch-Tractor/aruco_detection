a498063c4475a12ffec57e87ff5f47d9c40fb5ff
debarghabhattofficial <dbhattacharjee@monarchtractor.com>
2024-12-20 01:10:38 +0530

    Update project with new scripts.

---
+++ src/aruco_app/src/source/camera.py
@@ -0,0 +1,153 @@
+"""
+This script contains the class definitions for the camera.
+The Camera class is used to represent the camera with its 
+intrinsic and distortion parameters.
+"""
+
+
+import numpy as np
+
+import rospy
+from sensor_msgs.msg import CameraInfo
+
+
+class Camera:
+    """
+    This class represents the camera with its 
+    intrinsic and distortion parameters.
+    """
+    def __init__(self,
+                 camera_name,
+                 camera_info_topic,
+                 use_default_intrinsics=False):
+        """
+        This class initializes the camera with intrinsic 
+        matrix and distortion coefficients.
+
+        Input:
+            - camera_name: Name of the camera
+            - intrinsic_matrix: Intrinsic camera matrix
+            - distortion_coefficients: Distortion coefficients
+        """
+        # Camera name and ROS topic
+        self.camera_name = camera_name
+        self.camera_info_topic = camera_info_topic
+
+        # Camera information
+        # Camera dimensions
+        self.camera_width = None
+        self.camera_height = None
+        # Camera parameters
+        self.intrinsic_matrix = None
+        self.distortion_vector = None
+
+        # Flag to use default intrinsic parameters
+        self.use_default_intrinsics = use_default_intrinsics
+
+        # Flag to check if the camera information is
+        # received.
+        self.information_received = False
+        # Timeout duration
+        self.timeout = 5.0
+
+        # Camera information topic subscriber
+        self.camera_info_sub = rospy.Subscriber(
+            name=self.camera_info_topic,
+            data_class=CameraInfo,
+            callback=self.camera_info_topic_callback
+        )
+
+        # Read camera information.
+        if not self.use_default_intrinsics:
+            self.wait_for_information()
+            if not self.information_received:
+                msg = "Using default camera parameters because " + \
+                "no camera parameter information was received."
+                rospy.loginfo(msg)
+                self.use_default_intrinsics = True
+                self.use_default_camera_parameters()
+        else:
+            self.use_default_camera_parameters()
+
+        # Print camera information.
+        print(f"intrinsic_matrix shape: {self.intrinsic_matrix.shape}")
+        print(f"intrinsic_matrix: \n{self.intrinsic_matrix}")
+        print("-" * 75)
+        print(f"distortion_vector shape: {self.distortion_vector.shape}")
+        print(f"distortion_vector: \n{self.distortion_vector}")
+        print("-" * 75)
+
+    def use_default_intrinsics_type1(self):
+        """
+        This method returns default camera intrinsic parameters 
+        (type 1).
+        """
+        # Intrinsic matrix
+        self.intrinsic_matrix = np.array([
+            [1093.27,     0.0,  965.0],
+            [    0.0, 1093.27,  569.0],
+            [    0.0,     0.0,    1.0]
+        ], dtype=np.float32)
+        # Distortion coefficients
+        self.distortion_vector = np.array(
+            [0.0, 0.0, 0.0, 0.0, 0.0],
+            dtype=np.float32
+        )
+        return
+
+    def camera_info_topic_callback(self, msg):
+        """
+        This callback method decodes the camera information
+        from the cameradistortion_vector information topic if not already
+        received.
+        """
+        if not self.information_received:
+            rospy.loginfo("Received camera information.")
+            # Set flag to True.
+            self.information_received = True
+            # Extract camera information.
+            # Extract camera dimensions.
+            self.camera_width = msg.width
+            self.camera_height = msg.height
+
+            # Extract camera parameters.
+            self.intrinsic_matrix = np.array(
+                msg.K,
+                dtype=np.float32
+            ).reshape((3, 3))
+            self.distortion_vector = np.array(
+                msg.D,
+                dtype=np.float32
+            )
+
+            # Use default values for camera intrinsic and
+            # extrinsic parameters if flags are set to true.
+            self.use_default_camera_parameters()
+
+    def use_default_camera_parameters(self):
+        """
+        This method returns default camera intrinsic and 
+        extrinsic parameters.
+        """
+        if self.use_default_intrinsics:
+            self.use_default_intrinsics_type1()
+
+        return
+
+    def wait_for_information(self):
+        """
+        This method waits for the camera information to be
+        received.
+        """
+        start_time = rospy.Time.now()
+        while (rospy.Time.now() - start_time) < rospy.Duration(self.timeout):
+            if self.information_received:
+                rospy.loginfo("Camera information received within the timeout.")
+                return
+            rospy.sleep(0.1)  # Sleep to avoid busy-waiting
+        rospy.logwarn("Timeout reached without receiving camera information.")
+        # Unsubscribe from the topic
+        self.camera_info_sub.unregister()
+        rospy.loginfo("Unsubscribed from the topic.")
+        return
+    
---
+++ src/aruco_app/src/source/detector.py
@@ -0,0 +1,173 @@
+"""
+This script contains the code to detect ArUco 
+markers and estimate their poses.
+"""
+
+
+from concurrent.futures import ThreadPoolExecutor
+
+import numpy as np
+import cv2
+
+
+class Detector:
+    """
+    This class handles the detection of 
+    ArUco markers and their pose estimation.
+    """
+    def __init__(self,
+                 aruco_dict_type=cv2.aruco.DICT_6X6_250):
+        """
+        Initialize the detector with the specified ArUco 
+        dictionary type.
+
+        Input:
+            - aruco_dict_type: Type of ArUco dictionary
+        """
+        aruco_dict = cv2.aruco.getPredefinedDictionary(
+            aruco_dict_type
+        )
+        parameters = cv2.aruco.DetectorParameters()
+        self.aruco_detector = cv2.aruco.ArucoDetector(
+            aruco_dict, parameters
+        )
+        # Resized image
+        self.resized_img = None
+
+    def detect_markers(self):
+        """
+        This method detects ArUco markers in 
+        the input image.
+
+        Output:
+            - Detected marker corners, IDs, and 
+              rejected points
+        """
+        if self.resized_img is None:
+            raise ValueError("Resized image is not set yet.")
+
+        gray = cv2.cvtColor(self.resized_img, cv2.COLOR_BGR2GRAY)
+        corners, ids, rejected_img_points = \
+            self.aruco_detector.detectMarkers(gray)
+        return (
+            corners,
+            ids,
+            rejected_img_points
+        )
+
+    def estimate_pose(self,
+                      corner,
+                      marker_points,
+                      intrinsic_matrix,
+                      distortion_vector):
+        """
+        This method is a helper function to estimate pose for 
+        a single marker.
+
+        Input:
+            - corner: Detected marker corner
+            - marker_points: 3D coordinates of marker point
+            - intrinsic_matrix: Camera intrinsic parameters
+            - distortion_vector: Camera distortion coefficients
+
+        Output:
+            - Rotation and translation vectors (rvec, tvec)
+        """
+        _, rvec, tvec = cv2.solvePnP(
+            marker_points,
+            corner,
+            intrinsic_matrix,
+            distortion_vector
+        )
+        return rvec, tvec
+
+    def estimate_pose_multiple(self,
+                               corners,
+                               marker_length,
+                               intrinsic_matrix,
+                               distortion_vector):
+        """
+        This method estimates the poses of multiple markers.
+
+        Input:
+            - corners: Detected marker corners
+            - marker_length: Physical length of the marker 
+                  in meters
+            - intrinsic_matrix: Camera intrinsic parameters
+            - distortion_vector: Camera distortion coefficients
+        
+        Output:
+            - Rotation and translation vectors for each 
+              marker
+        """
+        # Define the 3D coordinates of the marker points.
+        marker_points = np.array([
+            [[0, 0, 0]],  # Top-left corner as the origin
+            [[0, marker_length, 0]],  # Bottom-left
+            [[marker_length, marker_length, 0]],  # Bottom-right
+            [[marker_length, 0, 0]]  # Top-right
+        ], dtype=np.float32)
+
+        with ThreadPoolExecutor() as executor:
+            results = list(executor.map(
+                lambda corner: self.estimate_pose(
+                    corner,
+                    marker_points,
+                    intrinsic_matrix,
+                    distortion_vector
+                ),
+                corners
+            ))
+        rvecs, tvecs = zip(*results)
+
+        return rvecs, tvecs
+
+    def draw_axes(self,
+                  rvec,
+                  tvec,
+                  intrinsic_matrix,
+                  distortion_vector,
+                  axis_length=0.05):
+        """
+        This method draws coordinate axes on the 
+        image for the given pose.
+
+        Input:
+            - img: Input image
+            - rvec: Rotation vector
+            - tvec: Translation vector
+            - camera: Camera object
+            - axis_length: Length of the axes to be drawn
+        """
+        # Reshape rvec and tvec to ensure compatibility.
+        rvec = np.array(rvec, dtype=np.float32).reshape(1, 3)
+        tvec = np.array(tvec, dtype=np.float32).reshape(1, 3)
+
+        # Draw axes.
+        cv2.drawFrameAxes(
+            self.resized_img,
+            intrinsic_matrix,
+            distortion_vector,
+            rvec,
+            tvec,
+            axis_length
+        )
+
+        return
+
+    def draw_detected_markers(self,
+                              corners):
+        """
+        This method draws detected markers 
+        on the image.
+
+        Input:
+            - corners: Detected marker corners
+        """
+        # Draw detected markers on the resized image.
+        cv2.aruco.drawDetectedMarkers(
+            self.resized_img,
+            corners
+        )
+        return
+    
